1fd4427cf1021505b97988955eac53742a92cd4a
Matt Du-Feu <matt.dufeu@gmail.com>
2024-01-01 16:36:02 +0000

    Adding back imgSrc to projects as deploy failed

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -3,11 +3,13 @@ const projectsData = [
     title: 'TryHackMe',
     description: `I'm learning cyber security on TryHackMe. It's a lot of fun and a great deal of it is free.`,
     href: 'https://www.tryhackme.com',
+    imgSrc: ''
   },
   {
     title: 'Emulation - CHIP-8',
     description: `I wanted to write an emulator, and apparently CHIP-8 is the place to start. `,
     // href: '/blog/the-time-machine',
+    imgSrc: ''
   },
 ]
 
