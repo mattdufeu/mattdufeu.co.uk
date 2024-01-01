ebbd68bfd5ddd4c705d6de80467410e502dc2935
Matt Du-Feu <matt.dufeu@gmail.com>
2024-01-01 16:19:21 +0000

    Customize step 1 - basic details

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,19 +1,13 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'TryHackMe',
+    description: `I'm learning cyber security on TryHackMe. It's a lot of fun and a great deal of it is free.`,
+    href: 'https://www.tryhackme.com',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Emulation - CHIP-8',
+    description: `I wanted to write an emulator, and apparently CHIP-8 is the place to start. `,
+    // href: '/blog/the-time-machine',
   },
 ]
 
