5d6fe9cdf467783098cd731ad1bcc5498fa92aab
mattdufeu <matt.dufeu@gmail.com>
2025-10-14 13:01:26 +0100

    Porting old posts (#59)
    * Upgrading packages that don't cause errors
    * Updating the Licence
    * Posts to keep migrated across
    * Enable Umami

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,20 +7,9 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
-  },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: 'Exploring AI',
+    description: `I'm not going out on a limb by thinking that AI isn't going away. "Keep up or get left behind". From the underlying technology, to the latest AI news, I'm constantly researching how to get the most out of AI. (credit to https://image-generator.com/ for the image above)`,
+    imgSrc: '/static/images/researching-ai.png',
   },
 ]
 
